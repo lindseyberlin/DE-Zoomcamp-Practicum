5327e076980fe06fe122ef1c25d9bc8a0806cfd6
Lindsey <lberlinhale@gmail.com>
2025-01-22 12:39:47 -0600

    Add basic terraform files after initial demo

---
+++ 01_basics_setup/3_terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.17.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "carbon-pride-446318-e0"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "carbon-pride-446318-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
