575ad3727c452cadb283aedd1793fa66563175e6
Lindsey <lberlinhale@gmail.com>
2025-02-23 14:20:47 -0600

    Adjust dags and docker files in my quest to get airflow working

--- 02_data_ingestion/airflow/data_ingestion_gcs_dag.py
+++ 02_data_ingestion/airflow/data_ingestion_gcs_dag.py
@@ -1,54 +1,69 @@
+# Original Structure from the 2022 DTC Data Engineering Zoomcamp
+# Modern updates were made with reference to:
+# https://github.com/ManuelGuerra1987/data-engineering-zoomcamp-notes/blob/main/2_Workflow-Orchestration-AirFlow/airflow/dags/data_ingestion_gcp_yellow.py
+
 import os
 import logging
+import shutil
+import requests
+import gzip
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-
-from google.cloud import storage
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
-import pyarrow.csv as pv
-import pyarrow.parquet as pq
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+
+import pyarrow
+import pyarrow.csv
+import pyarrow.parquet
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
-dataset_file = "yellow_tripdata_2021-01.csv"
-dataset_url = f"https://s3.amazonaws.com/nyc-tlc/trip+data/{dataset_file}"
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-parquet_file = dataset_file.replace('.csv', '.parquet')
+
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'demo_dataset')
 
 
+def download(file_gz, file_csv, url):
+    # Download the CSV.GZ file
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_gz, 'wb') as f_out:
+            f_out.write(response.content)
+    else:
+        print(f"Error downloading file: {response.status_code}")
+        return False
+    
+    # Unzip the CSV file
+    with gzip.open(file_gz, 'rb') as f_in:
+        with open(file_csv, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
 def format_to_parquet(src_file):
-    if not src_file.endswith('.csv'):
-        logging.error("Can only accept source files in CSV format, for the moment")
-        return
-    table = pv.read_csv(src_file)
-    pq.write_table(table, src_file.replace('.csv', '.parquet'))
+
+    parquet_file = src_file.replace('.csv', '.parquet')
+    chunk_size=100000
+
+    csv_reader = pyarrow.csv.open_csv(src_file, read_options=pyarrow.csv.ReadOptions(block_size=chunk_size))
+
+    with pyarrow.parquet.ParquetWriter(parquet_file, csv_reader.schema) as writer:
+        for batch in csv_reader:
+            writer.write_batch(batch)    
+            print("another chunk inserted")
 
 
-# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
-def upload_to_gcs(bucket, object_name, local_file):
-    """
-    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
-    :param bucket: GCS bucket name
-    :param object_name: target path & file-name
-    :param local_file: source path & file-name
-    :return:
-    """
-    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
-    # (Ref: https://github.com/googleapis/python-storage/issues/74)
-    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
-    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
-    # End of Workaround
 
-    client = storage.Client()
-    bucket = client.bucket(bucket)
-
-    blob = bucket.blob(object_name)
-    blob.upload_from_filename(local_file)
+def upload_to_gcs(bucket, object_name, local_file, gcp_conn_id="gcp-airflow"):
+    hook = GCSHook(gcp_conn_id)
+    hook.upload(
+        bucket_name=bucket,
+        object_name=object_name,
+        filename=local_file,
+        timeout=600
+    )
 
 
 default_args = {
@@ -67,28 +82,42 @@ with DAG(
     max_active_runs=1,
     tags=['dtc-de'],
 ) as dag:
+    
+    table_name_template = 'yellow_taxi_{{ execution_date.strftime(\'%Y_%m\') }}'
+    file_template_csv_gz = 'output_{{ execution_date.strftime(\'%Y_%m\') }}.csv.gz'
+    file_template_csv = 'output_{{ execution_date.strftime(\'%Y_%m\') }}.csv'
+    file_template_parquet = 'output_{{ execution_date.strftime(\'%Y_%m\') }}.parquet'
+    # consolidated_table_name = "yellow_{{ execution_date.strftime(\'%Y\') }}"
+    url_template = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.csv.gz"
 
-    download_dataset_task = BashOperator(
+
+    download_dataset_task = PythonOperator(
         task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
+        python_callable=download,
+        op_kwargs={
+            'file_gz': file_template_csv_gz,
+            'file_csv': file_template_csv,
+            'file_parquet': file_template_parquet,
+            'url': url_template
+        },
     )
 
     format_to_parquet_task = PythonOperator(
         task_id="format_to_parquet_task",
         python_callable=format_to_parquet,
         op_kwargs={
-            "src_file": f"{path_to_local_home}/{dataset_file}",
+            "src_file": f"{path_to_local_home}/{file_template_csv}",
         },
+        retries=10,
     )
 
-    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
     local_to_gcs_task = PythonOperator(
         task_id="local_to_gcs_task",
         python_callable=upload_to_gcs,
         op_kwargs={
             "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
+            "object_name": f"raw/{file_template_parquet}",
+            "local_file": f"{path_to_local_home}/{file_template_parquet}",
         },
     )
 
@@ -102,7 +131,7 @@ with DAG(
             },
             "externalDataConfiguration": {
                 "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
+                "sourceUris": [f"gs://{BUCKET}/raw/{file_template_parquet}"],
             },
         },
     )
